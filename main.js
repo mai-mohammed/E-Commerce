454e076f35777e3baf9863443093125f617d73bf
Mai Mohammed <maymohammedabuabed@gmail.com>
2022-07-29 16:16:51 +0300

    Add html code and styles "it's not responsive"

---
+++ main.js
@@ -0,0 +1,51 @@
+const products = [
+  {
+    id: 0,
+    name: "Fiddle-leaf fig",
+    price: "110.99",
+    category: "decorar",
+    imageSrc: "https://images.unsplash.com/photo-1584589167171-541ce45f1eea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+  {
+    id: 1,
+    name: "Strelitzia nicolai",
+    price: "99",
+    category: "",
+    imageSrc: "https://images.unsplash.com/photo-1582131503261-fca1d1c0589f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+  {
+    id: 2,
+    name: "Aloe vera",
+    price: "90.50",
+    category: "decorar",
+    imageSrc: "https://images.unsplash.com/photo-1545239705-1564e58b9e4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+  {
+    id: 3,
+    name: "cactus",
+    price: "90.50",
+    category: "decorar",
+    imageSrc: "https://images.unsplash.com/photo-1551888419-c41351800459?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+  {
+    id: 4,
+    name: "Fiddle-leaf fig",
+    price: "110.99",
+    category: "decorar",
+    imageSrc: "https://images.unsplash.com/photo-1584589167171-541ce45f1eea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+  {
+    id: 5,
+    name: "Strelitzia nicolai",
+    price: "99",
+    category: "",
+    imageSrc: "https://images.unsplash.com/photo-1582131503261-fca1d1c0589f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+  {
+    id: 6,
+    name: "Aloe vera",
+    price: "90.50",
+    category: "decorar",
+    imageSrc: "https://images.unsplash.com/photo-1545239705-1564e58b9e4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  }
+];
